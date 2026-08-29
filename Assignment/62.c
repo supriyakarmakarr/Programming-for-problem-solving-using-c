5b6cf642fb661190eb4fa11de8f38e280b44871a
Supriya Karmakar <supriyakarmakar429@gmail.com>
2026-08-30 00:18:25 +0530

    Add files via upload

---
+++ Assignment/62.c
@@ -0,0 +1,18 @@
+// Write a program to print numbers divisible by both 3 and 5 between 1 and N.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter Number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            printf("%d \n", i);
+        }
+    }
+
+    return 0;
+}
