99af9c2fa22a28f95ce9eece4e89b03c309cf997
Supriya Karmakar <supriyakarmakar429@gmail.com>
2026-09-01 11:01:25 +0530

    Implement program to find second smallest digit

--- Assignment/91.c
+++ Assignment/91.c
@@ -1,3 +1,30 @@
 // Write a program to find the second smallest digit in a given number.
 
 
+#include <stdio.h>
+
+int main() {
+    int n, digit;
+    int small = 9, second = 9;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        digit = n % 10;
+
+        if (digit < small) {
+            second = small;
+            small = digit;
+        }
+        else if (digit < second) {
+            second = digit;
+        }
+
+        n = n / 10;
+    }
+
+    printf("Second smallest digit is %d", second);
+
+    return 0;
+}
