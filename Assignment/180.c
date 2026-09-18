31c684711e094ebc21d4a5fa7dce6b10b835a005
Supriya Karmakar <supriyakarmakar429@gmail.com>
2026-09-18 18:53:10 +0530

    Add program to create and display an integer array
    Implement a program to create and display an array from user input.

---
+++ Assignment/180.c
@@ -0,0 +1,28 @@
+//Write a Program that creates an array from a given list of integers.
+
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the integers:\n");
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
